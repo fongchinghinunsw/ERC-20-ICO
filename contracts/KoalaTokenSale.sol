c70ffd46a1fe80426b64570b6d36ca391e203474
Stephen Fong <fongchinghinunsw@gmail.com>
2022-06-12 21:22:13 +0800

    add token sale contract

---
+++ contracts/KoalaTokenSale.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./KoalaToken.sol";
+
+contract KoalaTokenSale {
+    address admin;
+    KoalaToken public tokenContract;
+    uint256 public tokenPrice;
+    uint256 public tokensSold;
+
+    event Sell(address _buyer, uint256 _amount);
+
+    constructor(KoalaToken _tokenContract, uint256 _tokenPrice) {
+        admin = msg.sender;
+        tokenContract = _tokenContract;
+        tokenPrice = _tokenPrice;
+    }
+
+    function buyTokens(uint256 _numberOfTokens) public payable {
+        require(msg.value == _numberOfTokens * tokenPrice);
+        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));
+
+        tokensSold += _numberOfTokens;
+
+        emit Sell(msg.sender, _numberOfTokens);
+    }
+
+    function endSale() public {
+        require(msg.sender == admin);
+        require(
+            tokenContract.transfer(
+                admin,
+                tokenContract.balanceOf(address(this))
+            )
+        );
+        selfdestruct(payable(admin));
+    }
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 const KoalaToken = artifacts.require("KoalaToken");
+const KoalaTokenSale = artifacts.require("./KoalaTokenSale.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(KoalaToken, 100000000);
+  deployer.deploy(KoalaToken, 100000000).then(function () {
+    var tokenPrice = 10000000000;
+    return deployer.deploy(KoalaTokenSale, KoalaToken.address, tokenPrice);
+  });
 };
