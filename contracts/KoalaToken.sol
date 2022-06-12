14b9a8d8c05683554d60472c0bf73d8129cc3ab1
Stephen Fong <fongchinghinunsw@gmail.com>
2022-06-12 11:19:33 +0800

    Implement delegated transfer

--- contracts/KoalaToken.sol
+++ contracts/KoalaToken.sol
@@ -8,7 +8,16 @@ contract KoalaToken {
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
     mapping(address => uint256) public balanceOf;
+    // mapping(A => mapping(B => k)) here means Account A approves Account B
+    // to spend k amount of tokens on his behalf
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor(uint256 _initialSupply) {
         balanceOf[msg.sender] = _initialSupply;
@@ -24,6 +33,31 @@ contract KoalaToken {
         balanceOf[_to] += _value;
 
         emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
+
         return true;
     }
 }
